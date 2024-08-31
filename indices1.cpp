da330bbcf0bffade3dedbd61c733d7703bc32ea5
Harsh877-tech <harsha.cholleti45@gmail.com>
2024-08-31 09:38:23 -0500

    new indices1.cpp

--- indices1.cpp
+++ indices1.cpp
@@ -4,7 +4,7 @@
 
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::vector<int> findIndices(std::vector<int>& nums, int target) {
         std::unordered_map<int, int> numMap;
         
         for (int i = 0; i < nums.size(); i++) {
@@ -20,7 +20,7 @@ public:
             numMap[nums[i]] = i;
         }
         
-        // If no soln is found
+        
         return {};
     }
 };
@@ -31,20 +31,21 @@ int main() {
     // Example 1
     std::vector<int> nums1 = {2, 7, 11, 15};
     int target1 = 9;
-    std::vector<int> result1 = solution.twoSum(nums1, target1);
+    std::vector<int> result1 = solution.findIndices(nums1, target1);
     std::cout << "Output for Example 1: [" << result1[0] << ", " << result1[1] << "]" << std::endl;
 
     // Example 2
     std::vector<int> nums2 = {3, 2, 4};
     int target2 = 6;
-    std::vector<int> result2 = solution.twoSum(nums2, target2);
+    std::vector<int> result2 = solution.findIndices(nums2, target2);
     std::cout << "Output for Example 2: [" << result2[0] << ", " << result2[1] << "]" << std::endl;
 
     // Example 3
     std::vector<int> nums3 = {3, 3};
     int target3 = 6;
-    std::vector<int> result3 = solution.twoSum(nums3, target3);
+    std::vector<int> result3 = solution.findIndices(nums3, target3);
     std::cout << "Output for Example 3: [" << result3[0] << ", " << result3[1] << "]" << std::endl;
 
     return 0;
 }
+
